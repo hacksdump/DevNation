359c203bbf15eb98e25dd536dae85adbacebab76
Harshit Maurya <hmaurya999@gmail.com>
2018-12-03 10:38:22 +0530

    Configure routes

---
+++ routes/web.php
@@ -0,0 +1,32 @@
+<?php
+
+/*
+|--------------------------------------------------------------------------
+| Web Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register web routes for your application. These
+| routes are loaded by the RouteServiceProvider within a group which
+| contains the "web" middleware group. Now create something great!
+|
+*/
+
+Route::get('/', 'HomeController@index');
+
+Route::get('post/{id}', 'PostController@getPost');
+
+Route::get('user/{username}', 'UserController@showProfile');
+
+Route::get('login', 'Auth\LoginController@showLogin');
+Route::post('login', 'Auth\LoginController@checkLogin');
+
+Route::get('register', 'Auth\LoginController@showRegister');
+Route::post('register', 'Auth\RegisterController@create');
+
+Route::get('ask', 'PostController@showPostCreator');
+Route::post('ask', 'PostController@createPost');
+
+Route::get('logout', "Auth\LoginController@logout");
+
+Route::post('answer', 'PostController@postAnswer');
+Route::get('answer/{question}', 'PostController@getAllAnswers');
