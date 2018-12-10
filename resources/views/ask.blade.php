2a68fd4d0ec1f09ba25feddf8d89f578b054f6bf
Harshit Maurya <hmaurya999@gmail.com>
2018-12-10 16:40:31 +0530

    Fix text box size

--- resources/views/ask.blade.php
+++ resources/views/ask.blade.php
@@ -1,8 +1,10 @@
 @extends('layout')
 
 @section('content')
+    <div class="container">
     {!! Form::open(['url' => 'ask']) !!}
     {!! Form::textarea('query', '', ['class' => 'form-control']) !!}
     {!! Form::submit('Submit', ['class' => 'form-control']) !!}
     {!! Form::close() !!}
+    </div>
 @stop
