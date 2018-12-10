79955032da2128340ef8299919ca807a49e028c2
Harshit Maurya <hmaurya999@gmail.com>
2018-12-10 16:10:12 +0530

    Update views to include upvote feature

--- resources/views/components/posts_list.blade.php
+++ resources/views/components/posts_list.blade.php
@@ -2,15 +2,16 @@
     @if($posts)
         @foreach($posts as $post)
             <div class="card container">
+                <a href="/post/{{$post['id']}}">
+                    {{ $post['question'] }}
+                </a>
                 <a href="/user/{{ $post['user']['username'] }}">
                     {{ $post['user']['name'] }}
                 </a>
                 <div>
                     {{ $post['created_at'] }}
                 </div>
-                <a href="/post/{{$post['id']}}">
-                    {{ $post['question'] }}
-                </a>
+
             </div>
         @endforeach
     @else
--- resources/views/post.blade.php
+++ resources/views/post.blade.php
@@ -16,6 +16,49 @@
             </p>
         </div>
         <script src="https://code.jquery.com/jquery-3.3.1.min.js"></script>
+        <script>
+            $.ajaxSetup({
+                headers: {
+                    'X-CSRF-TOKEN': $('meta[name="csrf-token"]').attr('content')
+                }
+            });
+
+            const updateAnswers = function(){
+                $.ajax({
+                    url: '/answer/{{ $post['id'] }}',
+                    type: 'GET',
+                    dataType: 'JSON',
+                }).done(function (response) {
+                    populateAnswers(response);
+                });
+            };
+
+            const populateAnswers = function(answerArray){
+                let answersContainer = $('#answersContainer');
+                answersContainer.children().remove();
+                $.each(answerArray, function(i, item) {
+                    answersContainer.append('<div class="card" id="' + answerArray[i]['id'] + '">' +
+                        '<div class="row upvote-container">' +
+                        '<button onclick="upvote(this)"' + 'class="' + (answerArray[i]['hasUpvoted']? "btn btn-success":"btn") + '"> ^ </button>' +
+                        '<span>' + answerArray[i]['upvotes'] + '</span>' +
+                        '</div>' +
+                        '<p>' + answerArray[i]['answer'] + '</p>' +
+                        '<span> answered by ' + answerArray[i]['name'] + '</span>' +
+                        '</div>');
+                });
+            };
+
+            $(window).on('load', function () {
+                updateAnswers();
+            });
+
+            setInterval(function () {
+                updateAnswers();
+            }, 5000);
+        </script>
+
+
+
         @if(Auth::check())
             <div class="card">
                 <div class="row">
@@ -23,12 +66,8 @@
                     <button class="btn btn-outline-primary col-2" id="postAnswer">Post</button>
                 </div>
             </div>
+
             <script>
-                $.ajaxSetup({
-                    headers: {
-                        'X-CSRF-TOKEN': $('meta[name="csrf-token"]').attr('content')
-                    }
-                });
                 const answerBox = $('#answerBox');
                 answerBox.on('keypress', function(event) {
                     if(event.which === 13){
@@ -59,37 +98,42 @@
                     }
                 }
 
+                const upvote = function(answerButton) {
+                    const answer_id = answerButton.parentElement.parentElement.getAttribute('id');
+                    $.ajax({
+                        url: '/upvote',
+                        type: 'POST',
+                        data: {answer_id: answer_id},
+                        dataType: 'JSON',
+                    }).done(function (response) {
+                        let currentUpvotes = document.getElementById(answer_id).getElementsByTagName('span')[0];
+                        currentUpvotes.innerHTML = parseInt(currentUpvotes.innerHTML) + response.result;
+                        let currentClass = answerButton.className;
+                        if (currentClass.indexOf('success') === -1){
+                            answerButton.classList.add('btn-success');
+                        }
+                        else {
+                            answerButton.classList.remove('btn-success');
+                        }
+
+                    });
+                };
 
             </script>
         @else
             <a href="/login"> Login </a> to answer
+            <script>
+                const upvote = function() {
+                    location.href = '/login';
+                };
+            </script>
         @endif
+
         <h4>Discussion</h4>
         <div id="answersContainer"></div>
     </div>
     <script>
-        const updateAnswers = function(){
-            $.ajax({
-                url: '/answer/{{ $post['id'] }}',
-                type: 'GET',
-                dataType: 'JSON',
-            }).done(function (response) {
-                populateAnswers(response);
-            });
-        };
-        const populateAnswers = function(answerArray){
-            answersContainer = $('#answersContainer');
-            answersContainer.children().remove();
-            $.each(answerArray, function(i, item) {
-                answersContainer.append('<div class="card">' + '<p>' + answerArray[i]['answer'] + '</p>answered by ' + answerArray[i]['name'] + '</div>');
-            });
-        };
-        $(window).on('load', function () {
-            updateAnswers();
-        });
-        setInterval(function () {
-            updateAnswers();
-        }, 5000);
+
 
     </script>
 
