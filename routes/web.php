ed23cfce6f96967864cc9a3c06c566c97eef7028
Harshit Maurya <hmaurya999@gmail.com>
2018-12-14 16:34:27 +0530

    Update routes for oauth paths

--- routes/web.php
+++ routes/web.php
@@ -21,7 +21,7 @@ Route::get('login', 'Auth\LoginController@showLogin');
 Route::post('login', 'Auth\LoginController@checkLogin');
 
 Route::get('register', 'Auth\LoginController@showRegister');
-Route::post('register', 'Auth\RegisterController@create');
+Route::post('register', 'Auth\LoginController@registerUser');
 
 Route::get('ask', 'PostController@showPostCreator');
 Route::post('ask', 'PostController@createPost');
@@ -32,3 +32,9 @@ Route::post('answer', 'PostController@postAnswer');
 Route::get('answer/{question}', 'PostController@getAllAnswers');
 
 Route::post('upvote', 'PostController@upvoteAnswer');
+
+Route::get('/redirect/{provider}', 'Auth\LoginController@redirectToProvider');
+Route::get('/callback/{provider}', 'Auth\LoginController@handleProviderCallback');
+
+Route::post('/register/username', 'Auth\LoginController@getUsernamePwdAfterOauth');
+Route::get('/register/username', 'Auth\LoginController@showRegister');
